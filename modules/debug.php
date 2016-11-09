3161a89bf1b0fc1a9aa94bdb0becf817380c6079
Alan <alan@webworksau.com.au>
2016-11-09 12:58:04 +1100

    Added krumo to templating so it can be used in template files for debugging

--- modules/debug.php
+++ modules/debug.php
@@ -7,8 +7,7 @@ class debug extends prefab {
 
 		if (admin::$signed)
 		{
-			$hive = $f3->hive();
-			
+			$hive = $f3->hive();	
 
 			$hive = $this->array_keys_blacklist($hive, ['HEADERS', 'GET', 'POST', 'COOKIE', 'REQUEST', 'SESSION', 'FILES', 'SERVER', 'ENV', 'CMS', 'ACE', 'DB', 'SETTINGS', 'AJAX','AGENT','ALIAS','ALIASES','AUTOLOAD','BASE','BITMASK','BODY','CACHE','CASELESS','CONFIG','CORS','DEBUG','DIACRITICS','DNSBL','EMOJI','ENCODING','ERROR','ESCAPE','EXCEPTION','EXEMPT','FALLBACK','FRAGMENT','HALT','HIGHLIGHT','HOST','IP','JAR','LANGUAGE','LOCALES','LOGS','ONERROR','ONREROUTE','PACKAGE','PARAMS','PATH','PATTERN','PLUGINS','PORT','PREFIX','PREMAP','QUERY','QUIET','RAW','REALM','RESPONSE','ROOT','ROUTES','SCHEME','SERIALIZER','TEMP','TIME','TZ','UI','UNLOAD','UPLOADS','URI','VERB','VERSION','XFRAME']);
 
@@ -19,6 +18,12 @@ class debug extends prefab {
 			$code .= "console.log({'Template Variables' : obj});";
 			$code .= "</script>";
 			$f3->set("debug", $code);
+
+
+			Template::instance()->filter("krumo", function ($array) {
+				require_once $GLOBALS["settings"]["paths"]["krumo"];
+				return krumo($array);
+			});
 		}
 		else
 		{
