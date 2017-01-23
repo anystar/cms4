5f6adb5cf20dacb16ed9289e3b3b9ebb61d19709
Alan <alan@webworksau.com.au>
2017-01-23 11:59:24 +1100

    added camelCase function to tools.php

--- tools/tools.php
+++ tools/tools.php
@@ -374,3 +374,16 @@ function determine_path ($f3) {
 	if (is_file($path))
 		$f3->FILE = $f3->PATH = "/".$path;
 }
+
+function camelCase($str, array $noStrip = [])
+{
+        // non-alpha and non-numeric characters become spaces
+        $str = preg_replace('/[^a-z0-9' . implode("", $noStrip) . ']+/i', ' ', $str);
+        $str = trim($str);
+        // uppercase the first character of each word
+        $str = ucwords($str);
+        $str = str_replace(" ", "", $str);
+        $str = lcfirst($str);
+
+        return $str;
+}
