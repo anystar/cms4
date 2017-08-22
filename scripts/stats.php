ee773bfe2922f31b2061f388ed9d7cec06294e4b
Summer <darklocker@gmail.com>
2017-08-22 10:49:20 +1000

    Implemented email alerts for view counter

--- scripts/stats.php
+++ scripts/stats.php
@@ -3,8 +3,11 @@
 class stats extends prefab {
 
 	public $stats;
+	private $email;
 
 	function __construct($settings) {
+
+		$this->email = $settings["user"];
 
 		// Create settings file
 		if (!is_file(".cms/stats.json"))
@@ -31,53 +34,26 @@ class stats extends prefab {
 
 				file_put_contents(".cms/stats.json", json_encode($this->stats, JSON_PRETTY_PRINT));
 
-				
-				
-				switch ($this->stats["views"])
-				{
-					case 100:
-						
-					break;
+				$milestones = array(
+					100=>"100 views",
+					1000=>"1000 views",
+					2000=>"2000 views",
+					5000=>"5k views",
+					10000=>"10k views",
+					25000=>"25k views",
+					50000=>"50k views",
+					100000=>"100k views",
+					250000=>"250k views",
+					500000=>"500k views",
+					750000=>"750k views",
+					1000000=>"1 Millon views!!!!",
+					2000000=>"Epic 2 Million views!",
+					5000000=>"Holy cow 5mil views!!",
+					10000000=>"wow 10 Million views!"
+				);
 
-					case 1000:
-
-					break;
-
-					case 2000:
-
-					break;
-
-					case 5000:
-
-					break;
-
-					case 10000:
-
-					break;
-
-					case 25000:
-
-					break;
-					case 50000:
-
-					break;
-
-					case 100000:
-
-					break;
-
-					case 250000:
-
-					break;
-
-					case 500000:
-
-					break;
-
-					case 500000:
-
-					break;
-				}
+				if (array_key_exists($this->stats["views"], $milestones))
+					$this->email_view_alert($milestones[$this->stats["views"]]);
 			}
 		}
 		
@@ -95,5 +71,57 @@ class stats extends prefab {
 
 			echo Template::instance()->render("/stats/stats.html", "text/html");
 		});
+	}
+
+	function email_view_alert ($view_message) {
+
+		$config = $GLOBALS["config"];
+		if (array_key_exists("stats", $config))
+		if (array_key_exists("emails", $config["stats"]))
+			$emails = $config["stats"]["emails"];
+
+		base::instance()->view_message = $view_message;
+		$webmaster_email_body = \Template::instance()->render("/stats/view_webmaster_alert_email.html", null);
+		$email_body = \Template::instance()->render("/stats/view_alert_email.html", null);
+		base::instance()->clear("view_message");
+
+		$config = base::instance()->CONFIG["mailer"];
+
+		// Webmaster emails
+		foreach ($emails as $email)
+		{
+			$smtp = new SMTP(
+							$config["smtp.host"],
+							$config["smtp.port"],
+							$config["smtp.scheme"],
+							$config["smtp.user"],
+							$config["smtp.pw"]
+						);
+
+			$smtp->set('To', $email);
+			$smtp->set('From', '"'.$config["smtp.from_name"].'"'.' <'.$config["smtp.from_mail"].'>');
+			$smtp->set('Subject', 'Milestone for '.base::instance()->HOST);
+			$smtp->set('Content-Type', "text/html");
+
+			$smtp->send($webmaster_email_body);
+		}
+
+		if ($this->email != "")
+		{
+			$smtp = new SMTP(
+							$config["smtp.host"],
+							$config["smtp.port"],
+							$config["smtp.scheme"],
+							$config["smtp.user"],
+							$config["smtp.pw"]
+						);
+
+			$smtp->set('To', $this->email);
+			$smtp->set('From', '"'.$config["smtp.from_name"].'"'.' <'.$config["smtp.from_mail"].'>');
+			$smtp->set('Subject', 'Milestone for '.base::instance()->HOST);
+			$smtp->set('Content-Type', "text/html");
+
+			$smtp->send($email_body);
+		}
 	}
 }
